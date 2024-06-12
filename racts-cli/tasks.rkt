dd26be149c7d451766473df89138fba24eeb469e
Eskinder Romha Fitsim <romha223@gmail.com>
2024-06-11 22:59:41 -0400

    configured task scheduler

--- racts-cli/tasks.rkt
+++ racts-cli/tasks.rkt
@@ -20,3 +20,28 @@
 
 ;;worker threads
 (define worker-threads '())
+
+;;function to submit a task
+(define (submit-task func args priority)
+  (let ([task-id (semaphore-wait task-id-counter)])
+    (set! task-id (add1 task-id))
+    (define new-task (task task-id func args priority 'pending))
+    (queue-add! task-queue new-task)
+    (semaphore-post task-id-counter)
+    (schedule-tasks)
+    task-id))
+
+;;function to get task status
+(define (get-task-status task-id)
+  (let loop ([queue task-queue])
+    (if (queue-empty? queue)
+        'not-found
+        (let ([task (queue-peek queue)])
+          (if (= (task-id task) task-id)
+              (task-status task)
+              (loop (queue-tail queue)))))))
+
+;;function to configure scheduler
+(define (configure-scheduler new-thread-pool-size)
+  (set! thread-pool-size new-thread-pool-size)
+  (schedule-tasks))
